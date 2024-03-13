40f7a17b9d1f01031b4366db2df8c74d2f173fdf
HR-OA-KTS 黄强 <670604@ky-tech.com.cn>
2024-03-13 22:47:10 +0800

    task#991925 feat: vitePress主题配置文件

---
+++ docs/.vitepress/menu/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author       : HR-OA-KTS 黄强 670604@ky-tech.com.cn
+ * @Date         : 2024-03-13 17:12:26
+ * @Description  : 文档路由对应的文件菜单
+ */
+
+import {
+  nav
+} from './nav'
+
+import {
+  H5Views,
+  H5Custom
+} from './sidebar'
+
+export {
+  nav,
+  H5Views,
+  H5Custom
+}
